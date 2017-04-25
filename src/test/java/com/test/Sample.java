f4f6110f9beb519ee67e51814a804e47648b4465
Pied Parker <piedparkerautomation>
2017-04-25 15:10:14 -0700

    First Commit

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,47 @@
+package com.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+import bsh.Capabilities;
+
+public class Sample {
+	IOSDriver<WebElement> driver;
+
+	@Test
+	public void sampleTest() {
+		driver.get("http://www.google.com");
+	}
+
+	@BeforeTest
+	public void beforeTest() throws MalformedURLException {
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "IOS");
+		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.3");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 6s");
+		cap.setCapability(MobileCapabilityType.BROWSER_NAME, "safari");
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+
+		driver = new IOSDriver<WebElement>(new URL(
+				"http://127.0.0.1:4723/wd/hub"), cap);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+}
